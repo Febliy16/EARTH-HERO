445bc5c0dc336a78cd704a1b07e67c46d4d13657
Febliy16 <141920424+Febliy16@users.noreply.github.com>
2025-05-05 00:26:56 +0700

    Add files via upload

---
+++ scriptlogin.js
@@ -0,0 +1,7 @@
+document.getElementById('Go').addEventListener('click', function() {
+    window.location.href = 'VOICE/index.html';
+})
+
+document.getElementById('dev').addEventListener('click', function() {
+    window.location.href = 'PROFIL/index.html'
+})
